b33df3c5c7bc31ef99303f1a14d23197c35cb63d
Pablo Coello <pablo@opseeker.com>
2021-10-03 19:31:16 +0200

    Setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup 
 import py2exe 
  
-setup(name="Nombre ejecutable", 
+setup(name="Kmeans", 
  version="1.0", 
- description="Breve descripcion", 
- author="autor", 
- author_email="email del autor", 
- url="url del proyecto", 
- license="tipo de licencia", 
- scripts=["nuestroArchivo.py, 
- console=["nuestroArchivo.py, 
+ description="", 
+ author="Pablo Coello Pulido", 
+ author_email="pablo.coello@outlook.es", 
+ url="https://github.com/PabloCoello/km-gui", 
+ license="", 
+ scripts=["main.py"], 
+ console=["main.py"], 
  options={"py2exe": {"bundle_files": 1}}, 
  zipfile=None,
 )
